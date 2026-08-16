73a99f4da9487781bb2b438d4ac132e206503292
jan-st0 <janstopyra9@gmail.com>
2026-08-16 13:22:49 +0200

    sql db functions

---
+++ include/sqlite_interface.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <sqlite3.h>
+#include <string_view>
+
+struct Mod {
+    int id;
+    std::string name;
+    std::string origin;
+    std::string path;
+};
+
+namespace sql {
+    using query = std::string_view;
+    inline constexpr query schema = R"sql(
+CREATE TABLE Mods (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    origin TEXT NOT NULL,
+    path TEXT
+);
+
+CREATE TABLE Presets (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE Preset_Link (
+    preset_id INTEGER NOT NULL,
+    mod_id INTEGER NOT NULL,
+    load_order INTEGER DEFAULT 0,
+    PRIMARY KEY (preset_id, mod_id),
+    FOREIGN KEY (preset_id) REFERENCES Presets(id) ON DELETE CASCADE,
+    FOREIGN KEY (mod_id) REFERENCES Mods(id) ON DELETE CASCADE
+) WITHOUT ROWID;
+
+)sql";
+    
+    inline constexpr query create_preset = R"sql(
+INSERT INTO Presets (name) VALUES(?);
+    )sql";
+}
+
+class StorageManager {
+private:
+    struct sqlite3 *db;
+    std::string_view dbPath;
+
+public:
+    StorageManager(const std::string& dbPath);
+    ~StorageManager();
+
+    void addMod(const Mod& mod);
+    std::vector<Mod> selectPreset(const std::string& name);
+    void initialize_schema();
+    void create_preset(std::string_view name);
+    void create_mods(const std::vector<Mod>& mods);
+};
--- src/storage/sqlite_interface.cpp
+++ src/storage/sqlite_interface.cpp
@@ -1,3 +1,51 @@
 #include "sqlite3.h"
+#include <iostream>
+#include <string>
+#include "sqlite_interface.hpp"
+#include <string_view>
+#include <format>
+#include <stdexcept>
+
+StorageManager::StorageManager(const std::string& path): dbPath(path), db(nullptr) {
+    
+    if (sqlite3_open(path.c_str(), &db) != SQLITE_OK) {
+        std::runtime_error("Failed to open DB");
+    }
+}
+
+StorageManager::~StorageManager() {
+    if (db != nullptr) {
+        sqlite3_close(db);
+        db = nullptr;
+    }
+}
 
 
+void StorageManager::initialize_schema() {
+    char* err_msg = nullptr;
+    int rc = sqlite3_exec(db, sql::schema.data(), nullptr, nullptr, &err_msg);
+
+    if (rc != SQLITE_OK) {
+        std::cerr << "Failed to initialize db with error: " << err_msg << "\n";
+        sqlite3_free(err_msg);
+    }
+}
+
+void StorageManager::create_preset(std::string_view name) {
+    sqlite3_stmt *stmt = nullptr;
+
+    if (sqlite3_prepare_v2(db, sql::create_preset.data(),
+                static_cast<int>(sql::create_preset.size()), 
+                &stmt, nullptr) != SQLITE_OK) {
+        std::cerr << "Failed to prepare statement: " << sqlite3_errmsg(db) << "\n";
+        return;
+    }
+    
+    sqlite3_bind_text(stmt, 1, name.data(), static_cast<int>(name.size()), SQLITE_STATIC);
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        std::cerr << "Execution failed: " << sqlite3_errmsg(db) << "\n";
+    }
+    
+    sqlite3_finalize(stmt);
+
+}
